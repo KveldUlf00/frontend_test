a7608121ca936a72c728d8121cb5a141ca550932
Blazej Raducki <raduckib@gmail.com>
2024-07-13 18:52:28 +0200

    feat: completed main and footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,5 +1,32 @@
+import PopupButton from "./reusable/PopupButton";
+
 const Footer = () => {
-  return <div className="container__footer">Footer</div>;
+  const popUpOptions = [
+    { value: 0, label: "zresetuj ustawienia" },
+    { value: 1, label: "pokaż dane osobowe" },
+  ];
+
+  const handleSelect = (value) => {
+    if (value) {
+      console.log("Selected: Pokaż dane osobowe");
+    } else {
+      console.log("Selected: Zresetuj ustawienia");
+    }
+  };
+
+  return (
+    <div className="container__footer">
+      <div className="cssDiv">
+        <span>CSS</span>
+        <span>is</span>
+        <span>awesome</span>
+      </div>
+      <div className="company-name">
+        <span>nabthat</span>
+      </div>
+      <PopupButton options={popUpOptions} onSelect={handleSelect} />
+    </div>
+  );
 };
 
 export default Footer;
